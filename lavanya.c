512bdeb104b15df64e7e5ac5367c8afc361b77e9
lavanyasaishankar <35295074+lavanyasaishankar@users.noreply.github.com>
2018-01-10 17:00:39 +0530

    Create lavanya.c

---
+++ lavanya.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int c,lowercase,uppercase;
+{
+printf("enter the alphabet");
+scanf("c",%c);
+}
+lowercase(c='a'||c='e'||c='i'||c='o'||c='u')
+uppercase(c='A'||c='E'||c='I'||c='O'||c='U')
+if(lowercase||uppercase)
+{
+printf("the given alphabets are consonant");
+}
+else
+{
+printf("the given alphabets are not consonant");
+}
+getch();
+}
