2ea46e06ffbb387c034adac4faf8d9da0c14240e
lavanyasaishankar <35295074+lavanyasaishankar@users.noreply.github.com>
2018-01-17 16:00:14 +0530

    VOWELS.C
    program to find it is a consonant or not

--- VOWELS.C
+++ VOWELS.C
@@ -7,8 +7,8 @@ int c,lowercase,uppercase;
 printf("enter the alphabet");
 scanf("c",%c);
 }
-lowercase(c='a'||c='e'||c='i'||c='o'||c='u')
-uppercase(c='A'||c='E'||c='I'||c='O'||c='U')
+lowercase(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+uppercase(c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
 if(lowercase||uppercase)
 {
 printf("the given alphabets are consonant");
