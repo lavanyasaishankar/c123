6d22029d789aa81a506dcd9940f8488328158a03
lavanyasaishankar <35295074+lavanyasaishankar@users.noreply.github.com>
2018-01-17 16:33:58 +0530

    POSITIVE.C
    program to find whether the given number is positive or negative

---
+++ POSITIVE.C
@@ -0,0 +1,18 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int n;
+print("enter the number")
+scanf("n",%d);
+}
+if(0>n)
+{
+printf("the given number is positive")
+}
+else
+{
+printf("the given number is negative")
+}
+getch();
+}
