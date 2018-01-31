d8a49571c236bfa287d4ddb41ca09180db8b839d
lavanyasaishankar <35295074+lavanyasaishankar@users.noreply.github.com>
2018-01-31 16:29:42 +0530

    INTEGER.C
    program to find sum of nth number of array

---
+++ INTEGER.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n[20],N,k,i,sum=0;
+printf("enter the 2 numbers");
+scanf("%d %d",&N,&k);
+printf("enter the no. of elements");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+	printf("enter the elements:");
+	scanf("%d",&n[i]);
+}
+for(i=0;i<=k;i++)
+       {
+         sum+=i;
+       }
+       printf("the sum is %d",sum);
+       getch();
+       }
