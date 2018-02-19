51c602b6ad25a1d2120bb6340a011647d657d19f
lavanyasaishankar <35295074+lavanyasaishankar@users.noreply.github.com>
2018-02-19 16:41:21 +0530

    NUMERIC.C
    validate if a string is a numeric

---
+++ NUMERIC.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    char la;
+    scanf("%c",&la);
+    if(la>='0' && la<='9')
+    {
+    printf("YES");
+    }
+    else
+    {
+    printf("NO");
+    }
+    return 0;
+}
