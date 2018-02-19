9d9251fba643cc7adfe72c708a6cbeefdd5af6bb
lavanyasaishankar <35295074+lavanyasaishankar@users.noreply.github.com>
2018-02-19 15:48:24 +0530

    ARMSTRONG.C
    find whether the given number is armstrong or not

---
+++ ARMSTRONG.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter the n values");
+scanf("%d",&n);
+for(n=0;n<1;n++)
+{
+if(a*2==0)
+{
+printf("this is armstrong number");
+}
+else
+{
+printf("not armstrong number");
+}
+}
+}
