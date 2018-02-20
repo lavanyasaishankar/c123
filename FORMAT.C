43fb66b3368d470799bfdb201870a5a99136ed20
lavanyasaishankar <35295074+lavanyasaishankar@users.noreply.github.com>
2018-02-20 19:19:38 +0530

    FORMAT.C
    given format for hr and min print the same

---
+++ FORMAT.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int A1,A2,A3,min1,min2,min3,hr1,hr2,hr3;
+printf("enter the first time in hr and min");
+scanf("%d%d",&hr1,&min1);
+printf("enter the second time in hr and min");
+scanf("%d%d",&hr2,&min2);
+A1=(hr1*60)+min1;
+A2=(hr2*60)+min2;
+A3=t1-t2;
+hr3=t3/60;
+min3=t3%60;
+printf("difference between two times are  %d:%d",hr3,min3);
+}
