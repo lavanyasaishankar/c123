ee555e3f98231d8e6ccf5782aee66b8abb442a9c
lavanyasaishankar <35295074+lavanyasaishankar@users.noreply.github.com>
2018-02-21 16:02:47 +0530

    STRING.C
    count the numeric characters in the string

---
+++ STRING.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<string.h>
+int main(void)
+{
+	char l[50];
+	int i,count=0;
+	gets(a);
+	
+	for(i=0;l[i]!='\0';i++)
+	{
+		if(l[i]>='0' &&  l[i]<='9')
+		count++;
+	}
+	printf("%d",count);
+	return 0;
+}
