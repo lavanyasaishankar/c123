b24dcdc0d4891ed00a444b6d13ec4273631c5df2
lavanyasaishankar <35295074+lavanyasaishankar@users.noreply.github.com>
2018-03-02 15:58:52 +0530

    RANGE.C
    given n number find whether it is in range or not

---
+++ RANGE.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int l,a;
+printf("enter the value");
+scanf("%d",&a);
+for(l=1;l<=10;l++)
+{
+printf("%d",l);
+}
+if(a==l)
+{
+printf("yes");
+else
+{
+printf("no");
+}
+getch();
+}
