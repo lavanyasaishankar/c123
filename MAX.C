48c25f5aff647f0c5d5cbffa77ce9a5d8e7b8bb9
lavanyasaishankar <35295074+lavanyasaishankar@users.noreply.github.com>
2018-03-05 16:25:44 +0530

    MAX.C
    find maximum among ten numbers

---
+++ MAX.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() 
+{
+	int a[i],i,max;
+	printf("\n enter the ten numbers");
+	sacnf("%d",a[i]);
+	for(i=0;i>a[i];i++)
+	{
+		max=a[0];
+		if(a[i]>max)
+		{
+				max=a[i];
+			printf("the greatest among the ten numbers is %d",max);
+		}
+	}
+	
+	return 0;
+}
