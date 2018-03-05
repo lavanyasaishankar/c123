4f80dd5d5d51506e48816844e9459795cb167a13
lavanyasaishankar <35295074+lavanyasaishankar@users.noreply.github.com>
2018-03-05 15:58:03 +0530

    SAMEDIGIT.C
    print the same digit in same order

---
+++ SAMEDIGIT.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("\n enter the first numbers");
+	scanf("%d",&a);
+	printf("\n enter the second number");
+	scanf("%d",&b);
+	printf("\n entre the thrid number");
+	scanf("%d",&c);
+	printf("%d %d %d",a,b,c);
+	return 0;
+}
