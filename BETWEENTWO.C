8a6363c20340f2ea1993caff3f92a79af8d4403a
lavanyasaishankar <35295074+lavanyasaishankar@users.noreply.github.com>
2018-03-14 16:52:22 +0530

    BETWEENTWO.C
    check the given number is in betwen two intervals

---
+++ BETWEENTWO.C
@@ -0,0 +1,16 @@
+include<stdio.h>
+int main()
+{
+int a,l,s;
+printf("\n Enter a,l,s:");
+scanf("%d %d %d",&a,&l,&s);
+if(a>=l&&a<=s)
+{
+printf("\nYES");
+}
+else
+{
+printf("\nNO");
+}
+return 0;
+}
