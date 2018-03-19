9eb06461057cfc8e7be7b6ecc97fd38a4f33749e
lavanyasaishankar <35295074+lavanyasaishankar@users.noreply.github.com>
2018-03-19 16:32:18 +0530

    ISOGRAM.C
    check the given string is a isogram

---
+++ ISOGRAM.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char ch[100],a[100],b[100];
+gets(ch);
+int i,k;
+k=strlen(ch);
+for(i=0;i<k;i++)
+{
+if(i%2==0)
+printf("%c",ch[i]);	
+}
+printf(" ");
+for(i=0;i<k;i++)
+{
+if(i%2!=0)
+{
+printf("%c",ch[i]);
+}
+}
+return 0;
+}
