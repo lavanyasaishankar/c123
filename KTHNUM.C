d97eb184a21b2d6c05ae2ca2f26377fbedf9a5d3
lavanyasaishankar <35295074+lavanyasaishankar@users.noreply.github.com>
2018-03-21 16:02:22 +0530

    KTHNUM.C
    print the kth number

---
+++ KTHNUM.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a[10],n,l,i;
+printf("Enter the number of integers:")
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("Enter the position:")
+scanf("%d",&l);
+printf("%d",a[l-1]);
+}
