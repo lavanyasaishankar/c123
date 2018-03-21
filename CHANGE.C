f641284fa5a204c47e3bbfe8cecb940022aeb5ae
lavanyasaishankar <35295074+lavanyasaishankar@users.noreply.github.com>
2018-03-21 16:25:18 +0530

    CHANGE.C
    find its changed numbers

---
+++ CHANGE.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+int n,arr[10],i;
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+	scanf("%d",&arr[i]);
+	if(arr[i]!=i)
+	{
+	printf("%d",i);
+	break;
+	}
+	}
+return 0;
+}
