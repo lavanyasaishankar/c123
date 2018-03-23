78731a9212453156da4e83dd49e94079ada9faaf
lavanyasaishankar <35295074+lavanyasaishankar@users.noreply.github.com>
2018-03-23 15:53:37 +0530

    CAPITAL.C
    Find the sum of AP

---
+++ CAPITAL.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a,n,d,i,sum=0,next=0;
+scanf("%d %d %d",&a,&d,&n);
+next=a;
+for(i=0;i<n;i++)
+{
+	sum=sum+next;
+	next=next+d;	
+}
+printf("%d",sum);
+retrun 0;
+}
