6909d55a8cf658c6af5c3fcea61198206722eb7f
lavanyasaishankar <35295074+lavanyasaishankar@users.noreply.github.com>
2018-03-23 16:15:40 +0530

    DIVISION.
    find the value of d

---
+++ DIVISION.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	int a,b,c,d;
+	printf("\n enter the number 1");
+	scanf("%d",&a);
+	printf("\n enter the number 2");
+	scanf("%d",&b);
+    printf("\n enter the number 3");
+	scanf("%d",&c);
+	d=(a*b)/c;
+	printf("\n%d",d);
+	return 0;
+}
