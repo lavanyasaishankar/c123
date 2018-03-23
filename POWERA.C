d72224b31bcc2a1910a559ca610ad2e4a3a13c88
lavanyasaishankar <35295074+lavanyasaishankar@users.noreply.github.com>
2018-03-23 16:28:02 +0530

    POWERA.C
    find the power of the two numbers

---
+++ POWERA.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("\n enter the number 1");
+	scanf("%d",&a);
+	printf("\n enter the number 2");
+	scanf("%d",&b);
+            c=pow(a,b);
+	printf("\n%d",c);
+	return 0;
+}
